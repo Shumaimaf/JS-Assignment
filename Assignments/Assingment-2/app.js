9225da3f9fff537dee708ce2dae968fcaed45dfa
shumaimaf <shumaimaf@gmail.com>
2023-06-09 04:45:11 +0500

    initialize

--- Assignments/Assingment-2/app.js
+++ Assignments/Assingment-2/app.js
@@ -2,63 +2,63 @@
 
 // 1. Declare a variable called username. 
 
-// var userName = 'Shumaimaf';
-// console.log(userName);
+var userName = 'Shumaimaf';
+document.write(userName);
 
 // 2. Declare a variable called myName & assign to it a string 
 // that represents your Full Name.
 
-//  var myName = 'Shumaima Fatima'
-//  console.log(myName)
+ var myName = 'Shumaima Fatima'
+document.write(myName)
 
 // 3. Write script to
 // a) Declare a JS variable, titled message
 // b) Assign “Hello World” to variable message
 // c) Display the message in alert box
 
-// var message = 'Hello World';
-// alert(message);
+var message = 'Hello World';
+alert(message);
 
 // 4. Write a script to save student’s bio data in JS variables and 
 // show the data in alert boxes
 
-// var studentName = 'Jhone Doe';
-// var studentAge = '15 years old';
-// var mobApp = 'Certified Mob application development';
-// alert(studentName);
-// alert(studentAge);
-// alert(mobApp); 
+var studentName = 'Jhone Doe';
+var studentAge = '15 years old';
+var mobApp = 'Certified Mob application development';
+alert(studentName);
+alert(studentAge);
+alert(mobApp); 
 
 // 5. Write a script to display the following alert using one JS 
 // variable 
 
-// var myFood = ('Pizza\nPizz\nPiz\nPi\nP');
-// alert(myFood);
+var myFood = ('Pizza\nPizz\nPiz\nPi\nP');
+alert(myFood);
 
 // 6. Declare a variable called email and assign to it a string that 
 // represents your Email Address(e.g. example@example.com). 
 // Show the blow mentioned message in an alert box.(Hint: use 
 // string concatenation)
 
-// var eMail = 'Shumaimaf@gmail.com';
-// alert('My Email address is ' + eMail);
+var eMail = 'Shumaimaf@gmail.com';
+alert('My Email address is ' + eMail);
 
 // 7. Declare a variable called book & give it the value “A
 // smarter way to learn JavaScript”. Display the following
 // message in an alert box
 
-// var book = 'A smarter way to learn JavaScript';
-// alert('I am trying to learn from the book ' + book);
+var book = 'A smarter way to learn JavaScript';
+alert('I am trying to learn from the book ' + book);
 
 // 8. Write a script to display this in browser through JS
 
-// var myCode = 'Yah! I can write HTML content through JAVASCRIPT';
-// document.write(myCode);
+var myCode = 'Yah! I can write HTML content through JAVASCRIPT';
+document.write(myCode);
 
 // 9. Store following string in a variable and show in alert and 
 // browser through JS
 // “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
 
-// var message = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
-// alert(message);
+var message = '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+alert(message);
 
